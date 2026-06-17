ef53e0b7ca6603e01aaf01383cfaf0d4e9eb8110
dvdz03 <dflores04@alumnos.uaq.mx>
2026-06-17 09:45:24 -0600

    histograma

--- clase3-17JUN.R
+++ clase3-17JUN.R
@@ -1,1 +1,14 @@
 #Clase 17JUN
+rnorm(2)
+#simulación de eso
+agua<-rnorm(100000, mean=200, sd=50)
+hist(agua, freq = F)
+aguaD<-density(agua)
+lines(aguaD, col="purple", lwd=3)
+abline(v=250, col="blue", lwd=3)
+abline(v=150, col="blue", lwd=3)
+
+plot(aguaD, main = "Consumo de agua", xlab = "metros cúbicos", ylab = "densidad")
+abline(v=qnorm(0.025, 200, 50))
+abline(v=qnorm(0.975, 200, 50))
+#no siempre las muestras van a ser así de grandes entonces no vamos a obtener siempre así histogramas bonitos y asuich
