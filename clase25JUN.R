03a338749470c9faeb79ec367cd79352583c869f
dvdz03 <dflores04@alumnos.uaq.mx>
2026-06-25 12:28:24 -0600

    ancova

--- clase25JUN.R
+++ clase25JUN.R
@@ -53,23 +53,54 @@ summary(m0)#no se rechaza la hipótesis nula.
 ##SUPUESTOS, PRIMERO NORMALIDAD
 shapiro.test(m0$residuals)#si se distribuyen de manera normal
 bartlett.test(cathedral$x~cathedral$style)#las varianzas no son iguales
+#EMPEZAR CON TRANSFORMACIÓN DE VARIANZA
+m01<-aov(x^0.5 ~ style, data=cathedral)
+shapiro.test(m01$residuals)#si son normales, si no son normales es levene
+bartlett.test(x^0.5~style,data=cathedral)#no sirvió
+#AHORA EL INVERSO
+bartlett.test(x^-1~style,data=cathedral)
+#INVERSO RAÍZ
+bartlett.test(x^-0.5~style,data=cathedral)
+#LOGARITMO
+bartlett.test(log(x)~style,data=cathedral)
 
+#USAR LA FUNCIÓN LM SIN TRANSFORMAR variable explicativa cuantitativa continua y respuesta cuantitativa continua
+#le estoy poniendo una nominal, que hace R= convierte la nominal en variable dummy porque no puede usar las categóricas en regresión
+#la hace dummy pero pues asi bien asuich. 
+m02<-lm(x^0.9~style,data=cathedral)
+shapiro.test(m02$residuals)
+bartlett.test(log(x)~style,data=cathedral)
+#no queda de otra más que modelar la varianza
+#boxCox
+boxCox(m02,lambda=seq(-2,3,1/10))
 
 # ANCOVA
-m1 <- lm(y ~ x * style, data=cathedral)
-shapiro.test(m1$residuals)
-ncvTest(m1)
-durbinWatsonTest(m1)
+#faraway usa la lm para el ancova. ancho como variable respuesta y altura como explicativa y su covariable es el estilo
+m1 <- lm(y ~ x * style, data=cathedral)#ya con esto mejoró el problema de las varianzas
+shapiro.test(m1$residuals)#no es normal
+ncvTest(m1)#no hay heterocisticidad 
+durbinWatsonTest(m1)#datos independientes
+boxCox(m1,lambda=seq(-2,3,1/10))
+#transformación raíz cuadrada
+m1 <- lm(y^1.5 ~ x * style, data=cathedral)#con raíz no salió pero con 1.5 si
+summary(m1)#la ordenada al orgien no es significativa o sea que el modelo pasa por el origen.
+#la beta 0 (ordenada al origen) pasa por el origen esa es la hipótesis nula
+#la altura es diferente a 0 si tiene un efecto porque el p valor es menor a 0.05
+#el estilo no importa y tampoco la interacción
+#47% de explicación, no tan bueno asuich
+#como la interacción tiene el valor de p más grande se elimina del modelo adiós 
 
-summary(m1)
 
-
-m2<- lm(y ~ x + style, data=cathedral)
-shapiro.test(m2$residuals)
+m2<- lm(y ~ x + style, data=cathedral)#aquí se quita la interacción, solo se suma
+shapiro.test(m2$residuals)#no son normales, la desviación es muy pequeña
 ncvTest(m2)
 durbinWatsonTest(m2)
 
 summary(m2)
+#la ordenada al origen sigue siendo igual a 0, la pendiente de la altura es diferente de 0, el estilo es diferente de 0
+#mejoró la explicación, el ajuste del modelo ahora es de 49%
+#es un modelo de regresión en donde hay variables de diferente tipo
+
 
 # Nuestra conclusión es que para catedrales de la misma altura, las románicas son 8.39 pies más largas. 
 # Por cada pie adicional de altura, ambos tipos de catedral son aproximadamente 4,7 pies más largos. 
