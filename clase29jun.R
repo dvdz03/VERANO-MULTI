c0bd378a375496ad86c79e1feff581ea9c3d8069
dvdz03 <dflores04@alumnos.uaq.mx>
2026-06-29 12:12:17 -0600

    no se

--- clase29jun.R
+++ clase29jun.R
@@ -2,61 +2,124 @@
 pgfull<-read.table("C:/Users/100032608/Downloads/pgfull.txt", header=TRUE)
 pgfull
 head(pgfull)
-#análisis de factores, cuáles son los componentes principales, que factores ambientales  están asociados 
-library(GGally)
-ggpairs(pgfull)
+str(pgfull)
 
-pgfull2<-pgfull[,1:4]
-iris_2
-view(iris_2)
-especies<-iris$Species
+install.packages("EFAtools")
+library(EFAtools)
+library(tidyverse)
+install.packages("multiUS")
+library(multiUS)
 
-ggpairs(iris)#parece que las variables si tienen relación tal vez no perfecta pero si una relación, hay corrrelaciones significativas 
-#pero lo ideal es que todas lo tengan alv
-
-
-res.pca3 <- prcomp(iris[, -5],  scale = TRUE)
-
-fviz_eig(res.pca3)
-get_eig(res.pca3)
-
-fviz_pca_ind(res.pca3, label="none", habillage=iris$Species,
-             addEllipses=TRUE, ellipse.level=0.95, palette = "Dark2")
-
-
-fviz_pca_var(res.pca3,
-             col.var = "contrib", # Color by contributions to the PC
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE     # Avoid text overlapping
-)
-
-fviz_pca_biplot(res.pca3, lrepel=T, habillage=iris$Species, 
-                palette="Dark2",
-                col.var = "#080878") # Variables color
-
-
-# Eigenvalues
-eig.val <- get_eigenvalue(res.pca3)
-eig.val
-
-# Resultado para variables
-res.var <- get_pca_var(res.pca3)
-res.var$coord          # Coordenadas
-res.var$contrib        # Contribución  PCs
-res.var$cos2           # Calidad de la representación 
-
-# Resultados para individuos
-res.ind <- get_pca_ind(res.pca3)
-res.ind$coord          # Coordenadas
-res.ind$contrib        # Contribución  PCs
-res.ind$cos2           # Calidad de la representación
 
 
 #####################
-otroscp<-princomp(iris_2)
+# FASE 1: Objetivos #
+#####################
+# Serie de datos con variables para modelar las preferencias de riesgo de las personas
+# se empleó la escala de toma de riesgos de dominio específico (DOSPERT)
+# https://link.springer.com/article/10.1007/s11166-022-09398-5
+# https://osf.io/pjt57/
+
+##################
+# FASE 2: DISEÑO #
+##################
+
+# Los ítems miden la propensión a asumir riesgos 
+# en seis dominios diferentes: 
+# social, (soc)
+# recreativo, (rec)
+# juegos de azar, (rec)
+# salud/seguridad, (hear)
+# inversión y (fin)
+# ético. (eth)
+# FA-R
+
+# Subconjunto de los datos
+
+pg_especies<-pgfull[,1:54]#para las especies, las demás son las explicativas creo
+
+DOSPERT_sub <- DOSPERT_raw[1:500,]
+view(DOSPERT_sub)
+
+#####################
+# FASE 3: SUPUESTOS # 
+#####################
+BARTLETT(pg_especies)#correlación entre sí.
+KMO(pg_especies)#la adecuación de la muestra, arriba de 0.6 o 0.7 es bueno
+ani<-antiImage(pg_especies)
+print(ani,digits=3)
+
+BARTLETT(DOSPERT_sub)
+KMO(DOSPERT_sub)
+ai<-antiImage(DOSPERT_sub)
+print(ai, digits = 3)
+
+#################################
+# FASE 4: OBTENCIÓN DE FACTORES #
+#################################
+
+# Se empleará el método de análisis de factore comunes
+
+# Número de factores
+library(future)
+PARALLEL(pg_especies,eigen_type="SMC")
+
+PARALLEL(DOSPERT_sub, eigen_type = "SMC")
+?PARALLEL
+N_FACTORS(DOSPERT_sub, criteria = c("PARALLEL", "EKC", "SMT"),
+          eigen_type_other = c("SMC", "PCA"))
+N_FACTORS(DOSPERT_sub, method = "ULS")
+
+# 6 FACTORES
+
+EFA(DOSPERT_sub, n_factors = 6)
+Factores<-EFA(DOSPERT_sub, n_factors = 6)
+
+F1<-Factores$unrot_loadings[,1]
+F2<-Factores$unrot_loadings[,2]
+F3<-Factores$unrot_loadings[,3]
+F4<-Factores$unrot_loadings[,4]
+F5<-Factores$unrot_loadings[,5]
+F6<-Factores$unrot_loadings[,6]
+
+Facts<-data.frame(F1,F2,F3,F4,F5,F6)
+view(Facts)
+vars<-rownames(Facts)
+Facts<-mutate(Facts, vars=vars)
 
 
-PC1<-otroscp$scores[,1]
-PC2<-otroscp$scores[,2]
+Facts2 <- melt(Facts, id="vars", 
+               measure=c("F1","F2","F3","F4","F5","F6"), 
+               variable.name="Factor", value.name="Loading")
+view(Facts2)
 
-plot(PC1,PC2,pch=16, col=rainbow(3)[especies])
+ggplot(Facts2, aes(vars, abs(Loading), fill=Loading)) + 
+  facet_wrap(~ Factor, nrow=1) + 
+  geom_bar(stat="identity") + 
+  coord_flip() + 
+  scale_fill_gradient2(name = "Loading", 
+                       high = "blue", mid = "white", low = "red", 
+                       midpoint=0, guide=F) +
+  ylab("Magnitud de la carga") + 
+  theme_bw(base_size=10) 
+dominios<-c(rep("Ethic", 6), rep("Fin",6), rep("Hear",6),
+            rep("Recr",6), rep("Soc",6))
+Facts<-mutate(Facts, dominios=dominios)
+
+ggplot(Facts, aes(x=F1, y=F2, color=dominios)) +
+  geom_point() +
+  geom_hline(yintercept=0)+
+  geom_vline(xintercept = 0)
+
+
+##########################
+# FASE 5: INTERPRETACIÓN #
+##########################
+
+
+vmx1<-EFA(DOSPERT_sub, n_factors = 6, rotation = "varimax")
+
+
+pmx1<-EFA(DOSPERT_sub, n_factors = 6, rotation = "promax")
+
+EFA(DOSPERT_sub, n_factors = 6, rotation = "promax")
