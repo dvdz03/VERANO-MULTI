f7d61a950d3a38c01a8975d60f4557f239c5c06f
dvdz03 <dflores04@alumnos.uaq.mx>
2026-06-29 12:38:04 -0600

    reporte 8 incompleto

--- clase29jun.R
+++ clase29jun.R
@@ -62,8 +62,12 @@ print(ai, digits = 3)
 
 # Número de factores
 library(future)
+install.packages("parallelly",type="binary",dependencies = T)
+library(parallelly)
+library(EFAtools)
 PARALLEL(pg_especies,eigen_type="SMC")
-
+N_FACTORS(pg_especies,criteria=c("PARALLEL","EKC","SMT"),
+                       eigen_type_other = c("SMC","PCA"))
 PARALLEL(DOSPERT_sub, eigen_type = "SMC")
 ?PARALLEL
 N_FACTORS(DOSPERT_sub, criteria = c("PARALLEL", "EKC", "SMT"),
