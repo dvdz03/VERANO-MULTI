fc14ef61df9e9d40a9fb90a7ec71b3779d744253
dvdz03 <dflores04@alumnos.uaq.mx>
2026-07-05 23:29:30 -0600

    examen a casa

---
+++ examen7JUL.R
@@ -0,0 +1,17 @@
+#EXAMEN A CASA
+library(MVA)
+library(car)
+library(tidyverse)
+install.carinstall.packages("MVA")
+#EJERCICIO 1
+data("USairpollution")
+view(USairpollution)
+#EJERCICIO 2
+data("CHFLS")
+view(CHFLS)
+#EJERCICIO 3
+data("skulls")
+view(skulls)
+#EJERCICIO 4
+data("watervoles")
+view(watervoles)
