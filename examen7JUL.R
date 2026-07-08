257d1bbaec0456e85c55c4ce7c432dd89e4ed21d
dvdz03 <dflores04@alumnos.uaq.mx>
2026-07-07 20:44:23 -0600

    ejercicios examen

--- examen7JUL.R
+++ examen7JUL.R
@@ -24,6 +24,7 @@ ejercicio1.1<-step(ejercicio1)
 summary(ejercicio1.1)#62% de ajuste
 shapiro.test(ejercicio1.1$residuals)#no hay distribución normal 
 leveneTest(aire$SO2,data=aire)
+ncvTest(ejercicio1.1)
 plot(ejercicio1.1)
 library(car)
 vif(ejercicio1.1)#manufactura y población están altamente correlacionados
@@ -100,7 +101,7 @@ ej4final<- cbind(df_limpio, Cluster=as.factor(grupitos))
 #  )
 
 
-perfil_prom<-aggregate(cbind(R_age, R_income, as.numeric(R_health)) ~ Cluster, 
+perfil_prom<-aggregate(cbind(R_age, R_income,R_height, as.numeric(R_health)) ~ Cluster, 
                        data = ej4final, 
                        FUN = mean, na.rm = TRUE)
 conteos<-table(ej4final$Cluster)
@@ -122,75 +123,24 @@ library(fields)
 nonummuj<-ejercicio2[,c(1,3,5,7)]
 numuj<-ejercicio2[,c(2,4,6)]
 numuj1<-scale(numuj)
-distancias<-dist(numuj1)
-arbol<-hclust(distancias,method="ward.D2")
-plot(arbol,
-     main="tipos de mujeres",
-     xlab="encuestadas",
-     ylab="distancia",
-     sub="",
-     labels=F)
-tipos<-cutree(arbol,k=3)
-perfiles<-data.frame(tipologia=tipos,nonummuj)
-perfiles1<-aggregate(numuj,by=list(Tipo=tipos),FUN=mean)
-perfiles1
-table(perfiles$tipologia,perfiles[,1])
-#todos estos son observaciones de mujeres nada más
-ggpairs(nonummuj)
-ggpairs(numuj)
+
 regiones<-data.frame(nonummuj$R_region,numuj)
 regiones
+
+summarySE(ejercicio2,measurevar = "R_age")
+summarySE(ejercicio2,measurevar = "R_income")
+summarySE(ejercicio2,measurevar = "R_height")
+tapply(regiones,regiones$nonummuj.R_region,summary)
+summary(ejercicio2)
 edades<-tapply(regiones$R_age,regiones$nonummuj.R_region,median)
 ingresos<-tapply(regiones$R_income,regiones$nonummuj.R_region,median)
 alturas<-tapply(regiones$R_height,regiones$nonummuj.R_region,median)
-alturas
+
 barplot(sort(edades),las=2)
 barplot(sort(ingresos),las=2)
 barplot(sort(alturas),las=2)
 
-morfos<-data.frame(numuj)
-morfos1<-scale(morfos)
-dist_ancias<-dist(morfos1,method="euclidean")
-arbol1<-hclust(morfos1,method="ward.D2")
 
-#ANÁLISIS DE CONGLOMERADOS
-hclust()#variables continuas etiquetadas
-#necesitamos construir una base de datos que no tenga una columna cualitativa, necesitamos etiquetar nuestras filas porque necesitamos que la base de datos sea solo numérica
-climas<-data.frame(clima)#separar solo las variables climáticas
-rownames(climas)<-bichos$sitio#que los rownames fuera la variable sitio del bojeto bichos
-view(climas)
-#ahora si ya a hacer el modelo
-arbol<-hclust(dist(climas))
-plot(arbol)#aquí el grupo de sonora, zacatecas y sinaloa era el punto aislado en el pca, 
-#justo com ose vio en el pca, hay 3 climas 
-
-
-#### otro método
-ks<-kmeans(numuj1,centers=3,nstart=25)
-table(ks$cluster)
-aggregate(numuj,by=list(tipo=ks$cluster),FUN=mean)
-perfiles2<-data.frame(tipologia=ks$cluster,nonummuj)
-table(perfiles2$tipologia,perfiles2$R_region)
-
-#EJERCICIO 2
-as.factor(c(ejercicio2$R_region,ejercicio2$R_edu,ejercicio2$R_health,ejercicio2$R_happy))
-str(ejercicio2)
-library(cluster)
-dis_tan<-daisy(ejercicio2,metric="gower")
-hc<-hclust(dis_tan,method="ward.D2")
-plot(hc,main="holiwis")
-rect.hclust(hc,k=6,border="blue")
-grupos<-cutree(hc,k=6)
-oksi<-cbind(ejercicio2,tipomujer=as.factor(grupos))
-per_fil<-oksi %>%
-  group_by(tipomujer) %>%
-  summarise(
-    años=mean(R_age,na.rm=T),
-    ingreso=mean(R_income,na.rm=T),
-    salud=mean(as.numeric(R_health),na.rm=T),
-    feliz=mean(as.numeric(R_happy),na.rm=T)
-  )
-per_fil
 #EJERCICIO 3
 data("skulls")
 view(skulls)
@@ -202,7 +152,8 @@ tapply(calaveras$bh,calaveras$epoch,summary)
 tapply(calaveras$bl,calaveras$epoch,summary)
 tapply(calaveras$nh,calaveras$epoch,summary)
 #normalidad
-tapply(calaveras$mb,calaveras$epoch,shapiro.test)#el único donde hay 2 no normales
+tapply(calaveras$mb,calaveras$epoch,shapiro.test)
+#el único donde hay 2 no normales
 tapply(calaveras$bh,calaveras$epoch,shapiro.test)
 tapply(calaveras$bl,calaveras$epoch,shapiro.test)
 tapply(calaveras$nh,calaveras$epoch,shapiro.test)
@@ -225,6 +176,7 @@ summary(mode1)
 shapiro.test(mode1$residuals)
 bartlett.test(calaveras$mb~calaveras$epoch)#si todo bien
 TukeyHSD(mode1)
+calaveras$epoch
 respuestas<-cbind(calaveras$mb,calaveras$bh,calaveras$bl,calaveras$nh)
 mano_va<-manova(respuestas~epoch,data=calaveras)
 summary(mano_va)#si hay diferencias entre las épocas
@@ -234,15 +186,18 @@ res1<-aov(calaveras$mb~calaveras$epoch)
 res2<-aov(calaveras$bh~calaveras$epoch)
 res3<-aov(calaveras$bl~calaveras$epoch)
 TukeyHSD(res1)
+
 TukeyHSD(res2)
 TukeyHSD(res3)
 
 library(MASS)
 mod_lda<-lda(epoch~mb+bh+bl+nh,data=calaveras)
 mod_lda
-plot(mod_lda,dimen=1)
+plot(mod_lda,dimen = 1)
 post_hocs <- lda(calaveras$epoch~respuestas, CV=F)#análisis de discriminantes
 post_hocs
+
+
 #en conclusión si hubo mestizaje
 
 #EJERCICIO 4
@@ -253,7 +208,6 @@ matridist<-vegdist(watervoles1,method="euclidean")
 ahorasi<-metaMDS(matridist,k=2,trymax=100)
 #escalamiento multidimensional no métrico
 library(vegan)
-ejercicio4<-metaMDS(watervoles,k=2,trymax=100)
 grups<-c(rep("Reino Unido",6),rep("Europa",8))
 plot(ahorasi,type="n",main="NMDS")
 points(ahorasi,display="sites",col=ifelse(grups=="Reino Unido","red","blue"),pch=19)
